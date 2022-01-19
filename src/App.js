f5ac8583585e0da35d48898e7883014732e7380a
Marta Nara <marta.kos1230@gmail.com>
2022-01-19 15:03:21 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import Post from './components/pages/Post';
+import PostEdit from './components/pages/PostEdit';
+import PostAdd from './components/pages/PostAdd';
 
 const App = () => {
   return (
     <main>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
     </main>
   );
 }
